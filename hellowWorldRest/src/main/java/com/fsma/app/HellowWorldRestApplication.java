7668d34000afd3dff7c872a4da7dbb6b750bda66
Joao <jrdutra.com.br@gmail.com>
2019-02-20 16:47:19 -0300

    Primeiro Commit

---
+++ hellowWorldRest/src/main/java/com/fsma/app/HellowWorldRestApplication.java
@@ -0,0 +1,13 @@
+package com.fsma.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowWorldRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellowWorldRestApplication.class, args);
+	}
+
+}
